7d85e0b3e0c758a616605e722d038a2a91552384
cxf <1579814534@qq.com>
2020-02-17 23:20:21 +0800

    选择排序，插入排序

---
+++ src/main/java/com/cxf/algorithm/Selection.java
@@ -0,0 +1,51 @@
+package com.cxf.algorithm;
+
+/**
+ * 选择排序
+ */
+public class Selection {
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (less(a[i], a[i + 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void sort(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] a = {"S","O","R","T","E","X","A","M","P","L","E"};
+        sort(a);
+        isSorted(a);
+        show(a);
+    }
+}
