6a72272af6ceace624a8bccbba3756a1e1d3f207
cxf <1579814534@qq.com>
2020-02-18 00:34:43 +0800

    nothing

---
+++ src/main/java/com/cxf/algorithm/Stopwatch.java
@@ -0,0 +1,17 @@
+package com.cxf.algorithm;
+
+/**
+ * 时间计数器
+ */
+public class Stopwatch {
+    private final long start;
+
+    public Stopwatch() {
+        start = System.currentTimeMillis();
+    }
+
+    public double elapsedTime() {
+        long now = System.currentTimeMillis();
+        return (now - start) / 1000.0;
+    }
+}
