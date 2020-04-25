0af263c5e10e67346394653f0ce1d89dfe0a9e3c
cxf <1579814534@qq.com>
2020-04-25 23:18:51 +0800

    单链表

--- src/main/java/com/cxf/datastructures/linkedlist/SingleLinkedListDemo.java
+++ src/main/java/com/cxf/datastructures/linkedlist/SingleLinkedListDemo.java
@@ -6,16 +6,34 @@ package com.cxf.datastructures.linkedlist;
 public class SingleLinkedListDemo {
     public static void main(String[] args) {
         SingleLinkedList singleLinkedList = new SingleLinkedList();
+
+        //测试 -- 普通添加
 //        singleLinkedList.add(new Node(1, "宋江", "呼保义"));
 //        singleLinkedList.add(new Node(2, "卢俊义", "玉麒麟"));
 //        singleLinkedList.add(new Node(3, "吴用", "智多星"));
 //        singleLinkedList.add(new Node(4, "公孙胜", "入云龙"));
 
+        //测试 -- 按节点序号添加至链表
         singleLinkedList.addByNodeNo(new Node(1, "宋江", "呼保义"));
         singleLinkedList.addByNodeNo(new Node(4, "公孙胜", "入云龙"));
         singleLinkedList.addByNodeNo(new Node(3, "吴用", "智多星"));
         singleLinkedList.addByNodeNo(new Node(2, "卢俊义", "玉麒麟"));
         singleLinkedList.show();
+
+        System.out.println("-----------------------------------------");
+
+        //测试 -- 根据节点序号更新节点信息
+        Node node = new Node(2, "小卢", "玉麒麟~~");
+        singleLinkedList.update(node);
+        singleLinkedList.show();
+
+        System.out.println("-----------------------------------------");
+
+        //测试 -- 根据节点序号删除节点
+        singleLinkedList.delete(1);
+        singleLinkedList.show();
+
+
     }
 }
 
@@ -82,6 +100,65 @@ class SingleLinkedList {
             }
             temp = temp.next;
             System.out.println(temp);
+        }
+    }
+
+    /**
+     * 根据节点序号更新节点信息
+     *
+     * @param node
+     */
+    public void update(Node node) {
+        Node temp = headNode;
+        if (temp.next == null) {
+            System.out.println("链表为空");
+            return;
+        }
+        boolean flag = false;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            if (temp.no == node.no) {
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {
+            temp.name = node.name;
+            temp.nickName = node.nickName;
+        } else {
+            System.out.println("未找到对应节点");
+        }
+    }
+
+    /**
+     * 根据节点序号删除节点
+     *
+     * @param no
+     */
+    public void delete(int no) {
+        Node temp = headNode;
+        if (temp.next == null) {
+            System.out.println("链表为空");
+            return;
+        }
+        boolean flag = false;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            if (temp.next.no == no) {
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {
+            temp.next = temp.next.next;
+        } else {
+            System.out.println("未找到对应节点");
         }
     }
 }
