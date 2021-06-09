83f9fe64a9c2958165ac6f47f313a3ba13267980
陈晓峰 <chen1579814534@gmail.com>
2021-06-10 00:13:43 +0800

    [feat] leetcode -> add 006

---
+++ src/main/java/com/cxf/leetcode/Q_6.java
@@ -0,0 +1,35 @@
+package com.cxf.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q_6 {
+    public static void main(String[] args) throws Exception {
+        String str = "PAYPALISHIRING";
+        System.out.println(Q6Solution.convert(str, 3));
+    }
+}
+
+class Q6Solution {
+    public static String convert(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        List<StringBuilder> lst = new ArrayList<>();
+        for (int i = 0; i < Math.min(s.length(), numRows); i++) {
+            lst.add(new StringBuilder());
+        }
+
+        int sbIndex = 0;
+        boolean flag = false;
+        for (char c : s.toCharArray()) {
+            lst.get(sbIndex).append(c);
+            if (sbIndex == 0 || sbIndex == numRows - 1) flag = !flag;
+            sbIndex += flag ? 1 : -1;
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder stringBuilder : lst) res.append(stringBuilder);
+        return res.toString();
+    }
+}
