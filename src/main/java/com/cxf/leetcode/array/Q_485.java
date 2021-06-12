23e551533ec07ab98eec8e7a86dc32dfaa1d8b74
陈晓峰 <chen1579814534@gmail.com>
2021-06-12 22:53:42 +0800

    [feat] leetcode数组 -> add 485、495

---
+++ src/main/java/com/cxf/leetcode/array/Q_485.java
@@ -0,0 +1,23 @@
+package com.cxf.leetcode.array;
+
+public class Q_485 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(findMaxConsecutiveOnes(new int[]{1, 1, 0, 1, 1, 1}));
+    }
+
+    //一次遍历
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxNum = 0, currNum = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                currNum++;
+                continue;
+            }
+            maxNum = Math.max(currNum, maxNum);
+            currNum = 0;
+        }
+        //解决最后一个元素是1的情况
+        maxNum = Math.max(currNum, maxNum);
+        return maxNum;
+    }
+}
