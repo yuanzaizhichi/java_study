243a5a214aee7378219ed01deb9e161af063ab2b
陈晓峰 <chen1579814534@gmail.com>
2021-06-12 15:05:24 +0800

    [feat] leetcode -> add 009

---
+++ src/main/java/com/cxf/leetcode/Q_9.java
@@ -0,0 +1,46 @@
+package com.cxf.leetcode;
+
+public class Q_9 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Q9Solution2.isPalindrome(12321));
+    }
+}
+
+class Q9Solution {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        String str = Integer.toString(x);
+        int len = str.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - i - 1)) return false;
+        }
+        return true;
+    }
+}
+
+class Q9Solution2 {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int src = x;
+        long res = 0l;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) return false;
+        return src == res;
+    }
+}
+
+class Q9Solution3 {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int src = x;
+        long res = 0l;
+        while (x > res) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return x == res || x == res / 10;
+    }
+}
